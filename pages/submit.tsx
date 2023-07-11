1cd48f6f28015746852bb04cd944813927c750bb
Justin Sanford <justin.h.sanford@gmail.com>
2023-07-11 15:31:57 -0500

    say submissions are closed and remove airtable iframe

--- pages/submit.tsx
+++ pages/submit.tsx
@@ -2,15 +2,8 @@ export default function Home() {
 	return (
 		<div className="bg-white min-h-screen mt-12">
 			<div className="container max-w-screen-md mx-auto p-4 min-h-screen flex flex-col justify-center prose lg:prose-xl">
-				<h1>Submit a talk proposal for Demuxed 2023</h1>
+				<h1>Submissions for Demuxed 2023 are closed</h1>
 				<h2 className="mt-4">October 24th & 25th 2023, San Francisco</h2>
-				<iframe
-					className="airtable-embed"
-					src="https://airtable.com/embed/shrM3I6ta6f1P7osG?backgroundColor=white"
-					width="100%"
-					height="533"
-					style={{ background: 'transparent', border: '1px solid #ccc' }}
-				></iframe>
 				<div>
 					<h2>The Process</h2>
 					<p>
